459ba5f2a4bfbc25a5c419d9a1f98cd67018dcd2
Konstantin <kostnk1@gmail.com>
2024-01-28 07:44:08 +0100

    commit 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace hw15
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(", ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = classes.SelectMany(x => x.Students);
+
+            return allStudents.ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
